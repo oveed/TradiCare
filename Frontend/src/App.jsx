03fe12d0fee18819bf0d4472a11ce34bd20d0036
Ahmed Bensalem <ahmed.ben.salemy@gmail.com>
2024-07-20 01:21:53 +0100

    Added Log in page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Login from "./modules/Login";
 
 const App = () => {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Login />
     </div>
   );
 };
